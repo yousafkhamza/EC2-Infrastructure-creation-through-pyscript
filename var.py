25e7f7ef4ec0b9d2ac78c85d26929964bab87fc0
Yousaf K Hamza <31238282+yousafkhamza@users.noreply.github.com>
2022-03-20 09:17:19 +0530

    Update var.py

--- var.py
+++ var.py
@@ -1,4 +1,4 @@
-KEY_NAME = 'ec2-key-pair' # You can modify your Key pair name here
+PASSWORD = 'P@55W06D@12y' # You can modify your instance password here
 SG_NAME = 'FOREC2'        # You can modify your Security Group Name here
 IN_TYPE = 't2.micro'      # You can modify your instance type here
 IN_SIZE = '10'            # You can modify your instance size here
